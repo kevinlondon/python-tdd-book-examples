aed912bc71e48ba05263765508607f32d930a9c3
kevinlondon <kevinlondon@gmail.com>
2014-12-11 01:33:44 -0800

    First unit test from book and url mapping. Dummy view created.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        assert 1+1 == 3
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        assert found.func == home_page
