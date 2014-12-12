641c76ca2c5bc91de2e53058e36f9020b83d2a74
kevinlondon <kevinlondon@gmail.com>
2014-12-11 23:49:52 -0800

    Refactoring for the template to better match the functional tests. Test rendering the correct values.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,3 +17,16 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string("home.html")
         assert response.content.decode() == expected_html
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+        response = home_page(request)
+        assert "A new list item" in response.content.decode()
+
+        expected_html = render_to_string(
+            "home.html",
+            {"new_item_text": "A new list item"}
+        )
+        assert response.content.decode() == expected_html
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, "home.html", {
+        "new_item_text": request.POST.get("item_text", ""),
+    })
