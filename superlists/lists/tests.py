8001ce7dea50e71f092d253ed52e2ba8addd3284
kevinlondon <kevinlondon@gmail.com>
2014-12-11 21:48:03 -0800

    End of chapter 3, second unit test complete.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
@@ -8,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         assert found.func == home_page
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        assert response.content.startswith(b"<html>")
+        assert b"<title>To-Do lists</title>" in response.content
+        assert response.content.endswith(b"</html>")
