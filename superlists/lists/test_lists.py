e430c719dc5b9d4ae438780dda026153a4430d4a
kevinlondon <kevinlondon@gmail.com>
2014-12-13 17:24:30 -0800

    Add a URL for creating a new list via POST

--- superlists/lists/test_lists.py
+++ superlists/lists/test_lists.py
@@ -19,33 +19,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string("home.html")
         assert response.content.decode() == expected_html
 
-    def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest()
-        request.method = "POST"
-        new_item_text = "A new list item"
-        request.POST["item_text"] = new_item_text
-
-        response = home_page(request)
-
-        assert Item.objects.count() == 1
-        new_item = Item.objects.first()
-        assert new_item.text == new_item_text
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = "POST"
-        request.POST['item_text'] = "A new list item"
-
-        response = home_page(request)
-
-        assert response.status_code == 302
-        assert response['Location'] == "/lists/the-only-list-in-the-world/"
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        assert Item.objects.count() == 0
-
 
 class ListViewTest(TestCase):
 
@@ -61,6 +34,23 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'item1')
         self.assertContains(response, 'item2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_post_request(self):
+        data = {"item_text": "A new list item"}
+        response = self.client.post("/lists/new", data=data)
+
+        assert Item.objects.count() == 1
+        new_item = Item.objects.first()
+        assert new_item.text == "A new list item"
+
+    def test_redirects_after_POST(self):
+        data = {"item_text": "A new list item"}
+        response = self.client.post("/lists/new", data=data)
+
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ItemModelTest(TestCase):
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
         name="view_list"
     ),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
