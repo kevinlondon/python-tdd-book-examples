9851d22464a06a6de6d4ab04c9a5310969d20d72
kevinlondon <kevinlondon@gmail.com>
2014-12-14 22:15:09 -0800

    The first steps of the functional test for layout and style.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -78,3 +78,25 @@ class NewVisitorTest(LiveServerTestCase):
         assert "Buy milk" in page_text
 
         # Satisfied, they both go back to sleep.
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She noticed the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely cnetered there.
+        inputbox.send_keys("testing\n")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
