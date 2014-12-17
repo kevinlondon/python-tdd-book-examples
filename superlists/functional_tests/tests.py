feebb1cba636dfd706178253b9143e8c4aa159c1
kevinlondon <kevinlondon@gmail.com>
2014-12-16 23:10:53 -0800

    Hack functional test runner to test against staging.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,9 +6,24 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = "http://" + arg.split("=")[1]
+                return
+
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def tearDown(self):
         self.browser.quit()
@@ -19,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edit heard about a new online todo app. She goes to check out the homepage.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices page title and header mentions to-do lists
         assert 'To-Do' in self.browser.title
@@ -57,7 +73,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page and there is no sign of Edith's list.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name("body").text
         assert buy_peacock_text not in page_text
         assert make_fly_text not in page_text
@@ -81,7 +97,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She noticed the input box is nicely centered
