b08220add92a552b6e33a5609f6386f1d4de276a
kevinlondon <kevinlondon@gmail.com>
2014-12-26 19:10:47 -0800

    Split out unit tests into two files.

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -65,28 +65,6 @@ class NewListTest(TestCase):
         self.assertRedirects(response, '/lists/%d/' % new_list.pk)
 
 
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        lst = List()
-        lst.save()
-
-        first_text = "The first (ever) list item"
-        second_text = "Item the second"
-        first_item = Item.objects.create(text=first_text, list=lst)
-        second_item = Item.objects.create(text=second_text, list=lst)
-
-        saved_list = List.objects.first()
-        assert saved_list == lst
-
-        saved_items = Item.objects.all()
-        assert saved_items.count() == 2
-        assert saved_items[0].text == first_text
-        assert saved_items[0].list == lst
-        assert saved_items[1].text == second_text
-        assert saved_items[1].list == lst
-
-
 class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
