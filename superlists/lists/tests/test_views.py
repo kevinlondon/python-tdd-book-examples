a29a5505c8bfd34862dbbbd351793c6beeb36aa3
kevinlondon <kevinlondon@gmail.com>
2015-01-04 13:00:13 -0800

    Use new form in homepage, simplify tests.

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -6,19 +6,19 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
+    maxDiff = None
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        assert found.func == home_page
+    def test_home_page_renders_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string("home.html")
-        assert response.content.decode() == expected_html
+    def test_home_page_uses_item_form(self):
+        response = self.client.get("/")
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 
 class ListViewTest(TestCase):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
-from lists.models import Item, List
+from .forms import ItemForm
+from .models import Item, List
 
 def home_page(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"form": ItemForm()})
 
 
 def view_list(request, list_id):
