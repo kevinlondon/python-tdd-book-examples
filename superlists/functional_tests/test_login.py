7e988b4a2de6e4085ddff36c9bda7a162f779ae4
kevinlondon <kevinlondon@gmail.com>
2015-01-17 23:55:59 -0800

    Housekeeping

--- superlists/functional_tests/test_login.py
+++ superlists/functional_tests/test_login.py
@@ -27,7 +27,7 @@ class LoginTest(FunctionalTest):
         # Edith goes to the awesome superlists site
         # and notices a "sign in" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id("login").click()
+        self.browser.find_element_by_id("id_login").click()
 
         # A personal login box appears
         self.switch_to_new_window("Mozilla Persona")
@@ -41,6 +41,6 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window("To-Do")
 
         # She can see that she is logged in
-        self.wait_for_element_with_id("logout")
+        self.wait_for_element_with_id("id_logout")
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn("edit@mockmyid.com", navbar.text)
