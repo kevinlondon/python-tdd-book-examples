a47ac86f9d02b3da657d60e4c58a4f7873956743
kevinlondon <kevinlondon@gmail.com>
2015-05-19 21:02:18 -0700

    Clean up the template for the functional test and add in a logout option.

--- superlists/accounts/urls.py
+++ superlists/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
 from accounts import views
 
 urlpatterns = [
     url(r'^login$', views.persona_login, name='persona_login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
--- superlists/functional_tests/test_login.py
+++ superlists/functional_tests/test_login.py
@@ -2,6 +2,8 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from .base import FunctionalTest
 
+USER_EMAIL = 'edith@mockmyid.com'
+
 
 class LoginTest(FunctionalTest):
 
@@ -20,8 +22,21 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was:\n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(USER_EMAIL, navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(USER_EMAIL, navbar.text)
 
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
@@ -34,13 +49,24 @@ class LoginTest(FunctionalTest):
 
         # Edit logins in with her email address
         # # Use mockmyid.com for the test email
-        self.browser.find_element_by_id("authentication_email").send_keys("edit@mockmyid.com")
+        self.browser.find_element_by_id("authentication_email").send_keys(USER_EMAIL)
         self.browser.find_element_by_tag_name("button").click()
 
         # The persona window closes
         self.switch_to_new_window("To-Do")
 
         # She can see that she is logged in
-        self.wait_for_element_with_id("id_logout")
-        navbar = self.browser.find_element_by_css_selector(".navbar")
-        self.assertIn("edit@mockmyid.com", navbar.text)
+        self.wait_to_be_logged_in()
+
+        # Refreshing the page, she sees that it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # Terrified of this new feature, she reflexively clicks logout
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # The logged out status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
